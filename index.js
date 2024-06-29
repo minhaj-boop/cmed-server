58e110aa6633f121aaa7cdd9b5584b34aa593e51
minhaj-boop <imminhajj@gmail.com>
2024-06-29 13:18:39 +0600

    final

--- index.js
+++ index.js
@@ -6,14 +6,7 @@ const mongoose = require("mongoose")
 const connection = require('./db')
 const userRoutes = require('./routes/users')
 const authRoutes = require('./routes/auth')
-// const imageRoutes = require('./routes/images')
-// const fetchImagesRoutes = require('./routes/fetchImages')
-// const messageRoutes = require('./routes/messages')
-// const fetchMessagesRoutes = require('./routes/fetechAllMessages')
-// const fetchProduct = require('./routes/fetchProduct')
-// const deleteMessage = require('./routes/deleteMessage')
-// const deleteImage = require('./routes/deleteImage')
-// const updateImage = require('./routes/updateImage')
+
 
 //database connection
 connection();
@@ -25,14 +18,6 @@ app.use(cors())
 //routes
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
-// app.use('/api/upload-image', imageRoutes)
-// app.use('/api/all-images', fetchImagesRoutes)
-// app.use('/api/message', messageRoutes)
-// app.use('/api/all-messages', fetchMessagesRoutes)
-// app.use('/api/single-product', fetchProduct)
-// app.use('/api/delete-message', deleteMessage)
-// app.use('/api/delete-image', deleteImage)
-// app.use('/api/update-image', updateImage)
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, console.log("Server is listening at " + PORT))
